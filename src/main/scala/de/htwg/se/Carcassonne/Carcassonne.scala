cc267783f671cda0bf6871f7cd0301285fe4a12e
turboka11e <lukas.kaluscha@gmail.com>
2020-05-08 12:09:24 +0200

    Neuer Branch

--- src/main/scala/de/htwg/se/Carcassonne/Carcassonne.scala
+++ src/main/scala/de/htwg/se/Carcassonne/Carcassonne.scala
@@ -1,10 +1,12 @@
 package de.htwg.se.Carcassonne
 
-import de.htwg.se.Carcassonne.model.Player
+import de.htwg.se.Carcassonne.model.{Player, Spielfeld}
 
 object Carcassonne {
   def main(args: Array[String]): Unit = {
     val student = Player("Mark Boehme")
     println("Hello, " + student.name)
+    val spielfeld = Spielfeld(1)
+    print(spielfeld)
   }
 }
---
+++ src/main/scala/de/htwg/se/Carcassonne/model/Spielfeld.scala
@@ -0,0 +1,5 @@
+package de.htwg.se.Carcassonne.model
+
+case class Spielfeld(size: Int) {
+  print("Test"*size)
+}
