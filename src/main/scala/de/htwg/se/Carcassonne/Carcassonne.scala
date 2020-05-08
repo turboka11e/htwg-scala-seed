c654acded73e3a9ea13d60cad60dbf7fe8cd86fc
THE-FatTree <mark.vader.boehme@gmail.com>
2020-05-08 11:40:41 +0200

    Hi das ist ein TEST

--- src/main/scala/de/htwg/se/Carcassonne/Carcassonne.scala
+++ src/main/scala/de/htwg/se/Carcassonne/Carcassonne.scala
@@ -4,7 +4,7 @@ import de.htwg.se.Carcassonne.model.Player
 
 object Carcassonne {
   def main(args: Array[String]): Unit = {
-    val student = Player("Your Name")
+    val student = Player("Mark Boehme")
     println("Hello, " + student.name)
   }
 }
