79602d0cabd9cf93454f7e240f13d4cdd4ca081d
turboka11e <lukas.kaluscha@gmail.com>
2020-06-05 21:42:37 +0200

    Controller Tui - fork Digit entfernt und in Tui eingebaut

--- src/main/scala/de/htwg/se/Carcassonne/model/Points.scala
+++ src/main/scala/de/htwg/se/Carcassonne/model/Points.scala
@@ -4,17 +4,12 @@ case class Points() {
 
   def updatePoints(terriList:List[List[(Int, Area)]], players:List[Player]):List[Player] = {
 
-    //print(terriList)
-
-
     val closList = terriList.map(l => l.map(a => a._2.getCorners.size).map { case 1 => -1; case 2 => 0; case 3 => 1; case 4 => 2 })
     val reduced = closList.map(l => l.sum :: Nil)
     val openReduced = terriList.map(l => l.head._1).map{case 0 => 1; case _ => 0}.map(l => l.toDouble :: Nil)
     val openfinishedList = reduced.map(l => l.map { case -2 => 1.0; case 0 => 1.0; case _ => 0.0 })
 
     val finishedList = openReduced.zip(openfinishedList).map(l => l._1 ::: l._2).map(l => l.product :: Nil)
-
-
 
     val playList = terriList.map(l => l.map(a => a._2.player).distinct.filter(a => a != -1))
     val typeList = terriList.map(l => l.map(a => a._2.getValue).distinct.map { case 'c' => 2.0; case 'r' => 1.0; case _ => 0.0 })
