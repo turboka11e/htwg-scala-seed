c15393e5a323aa464bf53fa790077bc9cbf99bc9
turboka11e <lukas.kaluscha@gmail.com>
2020-06-10 15:07:28 +0200

    Points getested und in mehrere Methoden gepackt.

--- src/test/scala/de/htwg/se/Carcassonne/model/PointsSpec.scala
+++ src/test/scala/de/htwg/se/Carcassonne/model/PointsSpec.scala
@@ -3,34 +3,41 @@ package de.htwg.se.Carcassonne.model
 import org.scalatest._
 
 class PointsSpec extends WordSpec with Matchers {
-/*
-  "Territories grow in grid " when {
-    "Cards are placed next to another " should {
-      val emptyGrid = new Grid(3)
-      val oneCard = Card(List(Area('r', List('w', 'e'), Player("Road1")), Area('c', List('n')), Area('g', List('s'))))
-      val twoCard = Card(List(Area('r', List('w', 'e'), Player("Road2")), Area('c', List('n')), Area('g', List('s'))))
-      val threeCard = Card(List(Area('r', List('w', 'e'), Player("Road3")), Area('c', List('n')), Area('c', List('s'))))
-      val oneCardGrid = emptyGrid.place(1, 1, oneCard)
-      val twoCardGrid = oneCardGrid.place(0, 1, twoCard)
-      val threeCardGrid = twoCardGrid.place(1, 0, threeCard)
-      "and create Territory List" in {
-        oneCardGrid.getTerritories should be(List(List(Area('r', List('w', 'e'),
-          Player("Road1"))), List(Area('g', List('s'), Player("not selected"))), List(Area('c', List('n'), Player("not selected")))))
 
-        oneCardGrid.summonTerritories.toString should be("r 1 Road1\ng 1 not selected\nc 1 not selected\n")
+  "Points calculates the Points of the Player. Territories " when {
+    "processed in first Level " should {
 
-        twoCardGrid.getTerritories should be(List(List(Area('r', List('w', 'e'), Player("Road2")), Area('r', List('w', 'e'),
-          Player("Road1"))), List(Area('g', List('s'), Player("not selected"))), List(Area('c', List('n'), Player("not selected"))),
-          List(Area('g', List('s'), Player("not selected"))), List(Area('c', List('n'), Player("not selected")))))
+      val terriList:List[List[(Int, Area)]] = List(List((0, Area('c', List('n'), 0)), (0, Area('c', List('n'), 2))),
+        List((1, Area('r', List('n'), 2)), (2, Area('r', List('w', 'e', 's'), 1))), List((1, Area('g', List('n'), 2)), (0, Area('g', List('w'), -1))))
 
-        threeCardGrid.getTerritories should be(List(List(Area('r',List('w', 'e'), Player("Road3"))), List(Area('c',List('s'), Player("not selected")),
-          Area('c',List('n'), Player("not selected"))), List(Area('c',List('n'), Player("not selected"))), List(Area('r',List('w', 'e'), Player("Road2")),
-          Area('r',List('w', 'e'), Player("Road1"))), List(Area('g',List('s'), Player("not selected"))), List(Area('c',List('n'), Player("not selected"))),
-          List(Area('g',List('s'), Player("not selected")))))
+      val players = List(Player("Rick"), Player("Morty"), Player("Beth"))
 
+      val points = Points(terriList, players)
+
+      "return Lists of different calculated Parameters" in {
+
+        points.listOfFinishedTerris should be(List(List(1.0), List(0.0), List(0.0)))
+
+        points.playersToList should be(List(List(0, 2), List(2, 1), List(2)))
+
+        points.typeToPointsList should be(List(List(2.0), List(1.0), List(0.0)))
+
+        points.totalPointsOfTerris should be(List(List(4.0), List(2.0), List(0.0)))
+
+        points.reducedPointsOfTerris should be(List(4.0))
+
+        points.reducedPlayersOnTerri should be(List(2.0))
+
+        points.adjustedPointsForPlayers should be(List(2.0))
+
+        points.reducedPlayersList should be(List(List(0, 2)))
+
+        points.pointsForPlayersOnTerri should be(List((List(0,2), 2.0)))
+
+        points.updatePoints() should be(List(Player("Rick", 2.00), Player("Morty"), Player("Beth", 2.0)))
       }
     }
   }
 
- */
+
 }
