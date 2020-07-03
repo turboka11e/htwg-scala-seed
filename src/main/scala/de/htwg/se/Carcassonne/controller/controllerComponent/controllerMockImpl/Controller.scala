e8e3bd14de69879740e21f77d55469a04b271f9a
turboka11e <lukas.kaluscha@gmail.com>
2020-07-03 09:04:04 +0200

    ControllerMockImpl add

---
+++ src/main/scala/de/htwg/se/Carcassonne/controller/controllerComponent/controllerMockImpl/Controller.scala
@@ -0,0 +1,39 @@
+package de.htwg.se.Carcassonne.controller.controllerComponent.controllerMockImpl
+
+import com.google.inject.Inject
+import de.htwg.se.Carcassonne.controller.controllerComponent.ControllerInterface
+import de.htwg.se.Carcassonne.model.playfieldComponent.PlayfieldInterface
+
+class Controller @Inject() (var playfield: PlayfieldInterface) extends ControllerInterface {
+
+  def newGame():Unit = {}
+
+  def getPlayfield: PlayfieldInterface = playfield
+
+  def createGrid(size: Int):Unit = {}
+
+  def addPlayer(name: String): Unit = {}
+
+  def firstCard():Unit = {}
+
+  def firstCard(select: Int):Unit = {}
+
+  def changeGameState(gs: Int):Unit = {}
+
+  def getGameState:Int = 1
+
+  def rotateRight():Unit = {}
+
+  def rotateLeft():Unit = {}
+
+  def selectArea(nr: Int):Unit = {}
+
+  def placeCard(x:Int, y:Int):Unit = {}
+
+  def undo(): Unit = {}
+
+  def redo(): Unit = {}
+
+  def playFieldToString: String = ""
+
+}
