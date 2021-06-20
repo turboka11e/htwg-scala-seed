729935e78e81831f01b55ca835430219e3ccfab2
turboka11e <lukas.kaluscha@gmail.com>
2021-06-20 18:45:23 +0200

    added blueprint specs for FileIO

---
+++ FileIO/src/test/scala/de/htwg/se/Carcassonne/model/FileIOSpec.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.Carcassonne.model
+
+import org.scalatest._
+
+class FileIOSpec extends WordSpec with Matchers {
+
+  "+++ Blueprint +++" should {
+    val test = 1
+    "+++ Blueprint +++" in {
+      test should be(2)
+    }
+  }
+}
